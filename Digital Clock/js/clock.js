853ab2aa98604e09f88ec6a8c5196c3ce5eaba0c
RTefiki <116519669+RTefiki@users.noreply.github.com>
2023-01-09 22:24:14 +0100

    Add files via upload

---
+++ Digital Clock/js/clock.js
@@ -0,0 +1,31 @@
+
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+
+
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h > 24) {
+    h = h - 12;
+    ampm = "pm";
+  }
+
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl,(innerText = ampm);
+  setTimeout(()=>{
+  updateClock();
+},1000)
+}
+
+
+updateClock();
